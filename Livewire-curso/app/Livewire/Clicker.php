c7da09e849a1d7ace2904b5aacf4cae269799db6
GustavoCardoso-io <gustavoecardosom@gmail.com>
2024-02-27 20:23:28 -0300

    News

--- Livewire-curso/app/Livewire/Clicker.php
+++ Livewire-curso/app/Livewire/Clicker.php
@@ -2,12 +2,44 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Clicker extends Component
 {
+    use WithPagination;
+
+    #[Rule('required|string|min:2|max:255')]
+    public $name = '';
+    #[Rule('required|email|max:255|unique:users,email')]
+    public $email = '';
+    #[Rule('required|string|min:8')]
+    public $password = '';
+
+    public function createNewUser() {
+
+        $validated =  $this->validate();
+
+        User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        $this->reset(['name', 'email', 'password']);
+
+       session()->flash('success', 'Usuario cadastrado com successo');
+    }
+
     public function render()
     {
-        return view('livewire.clicker');
+        $users = User::paginate(5);
+
+        return view('livewire.clicker',[
+            'users' => $users
+        ]);
     }
 }
--- Livewire-curso/resources/views/livewire/clicker.blade.php
+++ Livewire-curso/resources/views/livewire/clicker.blade.php
@@ -1,3 +1,46 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="container">
+
+    @if (session('success'))
+        <span class="alert alert-primary mb-3" role="alert"> {{ session('success') }} </span>
+    @endif
+
+    <form wire:submit="createNewUser" action="">
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                wire:model.lazy="name">
+            @error('name')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                wire:model.lazy="email">
+            @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Senha</label>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password"
+                wire:model.lazy="password">
+            @error('password')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <button class="form-control btn btn-success">Criar</button>
+
+    </form>
+
+    @foreach ($users as $user)
+        <h3> {{ $user->name }} </h3>
+    @endforeach
+
+    {{ $users->links() }}
+
 </div>
